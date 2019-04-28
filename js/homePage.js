fb5b2e160180e21a324532b6cf5206bfaf760ab9
lucidlactose <jvillaruel@csumb.edu>
2019-04-28 03:25:34 +0000

    added a method for changing url based on search results in profile.php and all js files

--- js/homePage.js
+++ js/homePage.js
@@ -1,12 +1,15 @@
-Scout.configure({
-    clientId: "e90f5a13-6bc1-4d8d-b3aa-0724a4212d39"
-}).then(() => {
-    // Perform Scout queries here
+$(function() {
+    Scout.configure({
+        clientId: "e90f5a13-6bc1-4d8d-b3aa-0724a4212d39"
+    }).then(() => {
+        // Perform Scout queries here
+        
+    })
     
-})
-
-
-// Scout.titles.list().then(titles => console.log(titles));
-Scout.players.search("Ninja", "epic", null, "fortnite")
-  .then(search => Scout.players.get("fortnite", search.results[0].player.playerId))
-  .then(ninja => console.log(ninja))
+    username = $(this).data("username");
+    $("[name=search]").on("click", function() {
+        // Scout.titles.list().then(titles => console.log(titles));
+        window.location.href = "searchResults.php?username=" + username;
+    })
+     
+});
--- js/profilePage.js
+++ js/profilePage.js
@@ -1,15 +1,20 @@
-// Scout.configure({
-//     clientId: "e90f5a13-6bc1-4d8d-b3aa-0724a4212d39"
-// }).then(() => {
-//   // Perform Scout queries here
+Scout.configure({
+  clientId: "e90f5a13-6bc1-4d8d-b3aa-0724a4212d39"
+}).then(() => {
+  // Perform Scout queries here
     
-// })
+})
+    
+// this should be an on Click to
+// this is code that allows to subscrive to a player / follow it
 
-// // this is code that allows to subscrive to a player / follow it
-// let subscription = await Scout.players.subscribe("fortnite", "AQUACAGRzjVHkjKvTIpbF3ibQPec", "*"))
-// subscription.on("data", (player) => {
-//   console.log(player)
-// })
+function follow() {
+  let subscription = await Scout.players.subscribe("fortnite", "AQUACAGRzjVHkjKvTIpbF3ibQPec", "*"))
+  
+  subscription.on("data", (player) => {
+    console.log(player)
+  })
+}
 
 
 
