47e4ca25cdd8d04d646fc97a733907117485e1dc
lucidlactose <jvillaruel@csumb.edu>
2019-04-28 02:52:45 +0000

    changed profilesPage.js to profilePage.js

---
+++ js/profilePage.js
@@ -0,0 +1,22 @@
+// Scout.configure({
+//     clientId: "e90f5a13-6bc1-4d8d-b3aa-0724a4212d39"
+// }).then(() => {
+//   // Perform Scout queries here
+    
+// })
+
+// // this is code that allows to subscrive to a player / follow it
+// let subscription = await Scout.players.subscribe("fortnite", "AQUACAGRzjVHkjKvTIpbF3ibQPec", "*"))
+// subscription.on("data", (player) => {
+//   console.log(player)
+// })
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
