bf8741c358ad94d9155166d610e54cd8410c3d26
Enrique-Pesqueda <enrique7831@gmail.com>
2019-05-07 03:36:35 +0000

    gro

--- groupStats.php
+++ groupStats.php
@@ -11,6 +11,7 @@
         <script type="text/javascript" src="js/everyPage.js"></script>
         <script src="https://apis.google.com/js/platform.js" async defer></script>
         <meta name="google-signin-client_id" content="771332740040-bst02ajh5o98uga1dk3e36sv30pjknuh.apps.googleusercontent.com">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     </head>
     
 	<body>
