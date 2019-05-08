20c497ded97795e6f21e2956db018a4c1a963a4c
Jesus Villarruel <jvillaruel@csumb.edu>
2019-05-08 18:47:03 +0000

    added my heroku connection

--- API/dbConnection.php
+++ API/dbConnection.php
@@ -6,13 +6,13 @@ function getDatabaseConnection($dbname = "fortifyDB") {
     $password = "";
     
     //when connecting from Heroku
-    // if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-    //     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    //     $host = $url["host"];
-    //     $dbname = substr($url["path"], 1);
-    //     $username = $url["user"];
-    //     $password = $url["pass"];
-    // }
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    }
     
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
